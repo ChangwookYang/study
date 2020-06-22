35324361466389b93c64c450ad66b7afe337559b
ChangwookYang <ycw92325@gmail.com>
2020-06-22 23:39:22 +0900

    runner

---
+++ Runner.java
@@ -0,0 +1,17 @@
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42576?language=java
+ * 완주하지 못한 선수
+ * HashMap
+ */
+
+class Solution {
+    
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<Integer, String> participants = new HashMap<Integer, String>();
+
+        System.out.println("hello");
+        return answer;
+    }
+}
