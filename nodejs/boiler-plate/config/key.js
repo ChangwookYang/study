4c90b3e8f0062d975b024f4f03c4c46fe1ca8b5c
ChangwookYang <ycw92325@gmail.com>
2020-08-22 15:45:38 +0900

    secret config

---
+++ nodejs/boiler-plate/config/key.js
@@ -0,0 +1,7 @@
+// module.exports = require('./dev');
+
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
--- nodejs/boiler-plate/index.js
+++ nodejs/boiler-plate/index.js
@@ -1,15 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+
+// bodyParser client에서 오는 정보를 서버에서 분석할 수 있게함
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://changwook:ckddnr2@boilerplate.nz2y2.mongodb.net/boilerplate?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('Mongo DB connected...'))
   .catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!!!!!'))
+
+app.post('/register', (req, res) => {
+
+    // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
