fcd43b90e5cb4dd3e810c762cc89e4030dd2c167
ChangwookYang <ycw92325@gmail.com>
2021-03-21 23:39:18 +0900

    no message

--- servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -15,7 +15,7 @@ public class ResponseHeaderServlet extends HttpServlet {
         // [status-line]
         response.setStatus(HttpServletResponse.SC_OK);
 
-        response.setHeader("Content-Type", "text/plain");
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         response.setHeader("Pragma", "no-cache");
         response.setHeader("my-header", "hello");
